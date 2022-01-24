d999eecdf8b708608f33878c774d1a84108d6a7c
cmtzvitela <cmtzvitela@gmail.com>
2022-01-23 21:28:11 -0600

    Added a tentative footer. Created files for pages to be done

--- src/components/Common/Btncards.js
+++ src/components/Common/Btncards.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Btncards({className, buttonText}) {
     return (
-        <button className={`bg-secondary font-Poppins text-white rounded-full hover:bg-blue-400 px-10`}>
+        <button className={`bg-secondary font-Poppins text-white rounded-full hover:bg-blue-400 px-10 ${className}`}>
             {buttonText}
         </button>
     )
---
+++ src/pages/RouteSearch.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RouteSearch() {
+  return <div></div>;
+}
