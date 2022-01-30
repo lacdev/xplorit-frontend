e2a10debf1db579050533b2765c5039147265cbd
cmtzvitela <cmtzvitela@gmail.com>
2022-01-29 21:17:07 -0600

    Added Like Card for Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import DashboardCard from 'components/DashboardCard';
+import DashboardLikeCard from 'components/DashboardLikeCard';
 
 export default function Dashboard() {
   return <div>
     <DashboardCard/>
+    <DashboardLikeCard/>
   </div>;
 }
